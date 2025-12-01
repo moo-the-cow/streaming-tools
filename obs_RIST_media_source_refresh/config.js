762da41e0b7b5e50917bcb6f932cf898d55801ae
moo <34907770+moo-the-cow@users.noreply.github.com>
2025-12-01 13:22:06 +0100

    improved usability

---
+++ obs_RIST_media_source_refresh/config.js
@@ -0,0 +1,7 @@
+var config = {
+  "host": "127.0.0.1",
+  "port": "4455",
+  "mediaSourceName": "[CHANGE_ME_MEDIA_SOURCE_NAME]",
+  "refreshCooldownMs": 3000,
+  "statsUrl": "[CHANGE_ME_RIST-STATS-URL]:[CHANGE_ME_RIST-STATS-PORT]"
+};
